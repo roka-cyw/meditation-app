487a2827b0877679afb51dca36860b51b36a6439
Roka <kazakov302215@gmail.com>
2024-07-08 21:04:33 -0600

    setup project & added all config snipets

---
+++ app.d.ts
@@ -0,0 +1,2 @@
+// this string fix error when I use className for View container instead of styles={}
+/// <reference types='nativewind/types' />
---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+
+export default function HomeScreen() {
+  return (
+    <View className='flex-1 justify-center items-center'>
+      <Text>Hi app !</Text>
+    </View>
+  )
+}
