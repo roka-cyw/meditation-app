fb554fcd46c48b57c3f98387f78b404c7ef29648
Roka <kazakov302215@gmail.com>
2024-07-10 00:08:07 -0600

    render every affirmation info on the screen

--- app/(tabs)/affirmations/[itemId].tsx
+++ app/(tabs)/affirmations/[itemId].tsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react'
-import { View, Text } from 'react-native'
-import { useLocalSearchParams } from 'expo-router'
+import { View, Text, ImageBackground, Pressable, ScrollView } from 'react-native'
+import { useLocalSearchParams, router } from 'expo-router'
 
 import { GalleryPreviewData } from '@/constants/models/AffirmationCategory'
 import AFFIRMATION_GALLERY from '@/constants/affirmation-gallery'
+import AppGradient from '@/components/AppGradient'
+import { AntDesign } from '@expo/vector-icons'
 
 const AffirmationPractice = () => {
   const { itemId } = useLocalSearchParams()
   const [affirmation, setAffirmation] = useState<GalleryPreviewData>()
+  const [sentences, setSentences] = useState<string[]>([])
 
   useEffect(() => {
     for (let index = 0; index < AFFIRMATION_GALLERY.length; index++) {
@@ -17,14 +20,42 @@ const AffirmationPractice = () => {
 
       if (affirmationToStart) {
         setAffirmation(affirmationToStart)
+
+        const affirmationsArray = affirmationToStart.text.split('.')
+
+        if (affirmationsArray[affirmationsArray.length - 1] === '') {
+          affirmationsArray.pop()
+        }
+
+        setSentences(affirmationsArray)
+
         return
       }
     }
   }, [])
 
   return (
-    <View className='flex-1 justify-center items-center'>
-      <Text>AffirmationPractice</Text>
+    <View className='flex-1'>
+      <ImageBackground source={affirmation?.image} resizeMode='cover' className='flex-1'>
+        {/* I'll show gradient if image is absent */}
+        <AppGradient colors={['rgba(0, 0, 0, 0.3)', 'rgba(0, 0, 0, 0.9)']}>
+          <Pressable onPress={() => router.back()} className='absolute top-16 left-2 z-10'>
+            <AntDesign name='leftcircleo' size={40} color='white' />
+          </Pressable>
+
+          <ScrollView className='mt-20' showsVerticalScrollIndicator={false}>
+            <View className='h-full justify-center'>
+              <View className='h-4/5 mt-6 justify-center'>
+                {sentences.map((sentence, index) => (
+                  <Text key={index} className='text-white text-3xl mb-12 font-bold text-center'>
+                    {sentence}.
+                  </Text>
+                ))}
+              </View>
+            </View>
+          </ScrollView>
+        </AppGradient>
+      </ImageBackground>
     </View>
   )
 }
