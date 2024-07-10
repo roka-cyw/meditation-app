28def986d6650430007ba196cb7661ecb619161b
Roka <kazakov302215@gmail.com>
2024-07-10 01:11:02 -0600

    fixed issue with 00:09 instead of 00:00 formating on the timer

--- app/meditate/[id].tsx
+++ app/meditate/[id].tsx
@@ -31,6 +31,10 @@ const Meditate = () => {
     }
   }, [secondsRemaining, isMeditating])
 
+  // Format the time left to ensure two digits are displayed
+  const formattedTimeMinutes = String(Math.floor(secondsRemaining / 60)).padStart(2, '0')
+  const formattedTimeSeconds = String(secondsRemaining % 60).padStart(2, '0')
+
   return (
     <View className='flex-1'>
       <ImageBackground source={MEDITATION_IMAGES[Number(id) - 1]} resizeMode='cover' className='flex-1'>
@@ -41,7 +45,9 @@ const Meditate = () => {
 
           <View className='flex-1 justify-center'>
             <View className='mx-auto bg-neutral-200 rounded-full w-44 h-44 justify-center items-center'>
-              <Text className='text-4xl text-blue-800 '>00:{secondsRemaining}</Text>
+              <Text className='text-4xl text-blue-800 font-rmono'>
+                {formattedTimeMinutes}:{formattedTimeSeconds}
+              </Text>
             </View>
           </View>
 
